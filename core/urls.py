3322db3bec72fdf6eefb8e06e747aee569ff406d
Rafael Trindade <trindade.rafa@gmail.com>
2021-11-07 14:40:32 -0300

    Criando listas das classes exceto parametro

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,25 @@
 from django.urls import include, path
-from .views import home, lista_pessoas
+from .views import (home,
+                    lista_pessoas,
+                    lista_finalidades,
+                    lista_diaristas,
+                    lista_atividades_dia,
+                    lista_diaristas,
+                    lista_mensalistas,
+                    lista_atividades_mes,
+                    lista_anualistas,
+                    lista_atividades_ano,
+                    )
 
 
 urlpatterns = [
     path('', home, name='core_home'),
-    path('pessoas/', lista_pessoas, name='core_lista_pessoas')
+    path('pessoas/', lista_pessoas, name='core_lista_pessoas'),
+    path('finalidades/', lista_finalidades, name='core_lista_finalidades'),
+    path('diaristas/', lista_diaristas, name='core_lista_diaristas'),
+    path('atividades_dia/', lista_atividades_dia, name='core_lista_atividades_dia'),
+    path('mensalistas/', lista_mensalistas, name='core_lista_mensalistas'),
+    path('atividades_mes/', lista_atividades_mes, name='core_lista_atividades_mes'),
+    path('anualistas/', lista_anualistas, name='core_lista_anualistas'),
+    path('atividades_ano/', lista_atividades_ano, name='core_lista_atividades_ano'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
-from .models import Pessoa
+from .models import (Pessoa,
+                     Finalidade,
+                     Diarista,
+                     AtividadeDia,
+                     Mensalista,
+                     AtividadeMes,
+                     Anualista,
+                     AtividadeAno,
+                     )
 
 def home(request):
     context = {'mensagem': 'Ola mundo de novo'}
@@ -10,3 +18,38 @@ def home(request):
 def lista_pessoas(request):
     pessoas = Pessoa.objects.all()
     return render(request, 'core/lista_pessoas.html', {'usuario': pessoas})
+
+
+def lista_finalidades(request):
+    finalidade = Finalidade.objects.all()
+    return render(request, 'core/lista_finalidades.html', {'finalidades': finalidade})
+
+
+def lista_diaristas(request):
+    diarista = Diarista.objects.all()
+    return render(request, 'core/lista_diaristas.html', {'diaristas': diarista})
+
+
+def lista_atividades_dia(request):
+    atividade_dia = AtividadeDia.objects.all()
+    return render(request, 'core/lista_atividades_dia.html', {'atividades_dia': atividade_dia})
+
+
+def lista_mensalistas(request):
+    mensalista = Mensalista.objects.all()
+    return render(request, 'core/lista_mensalistas.html', {'mensalistas': mensalista})
+
+
+def lista_atividades_mes(request):
+    atividade_mes = AtividadeMes.objects.all()
+    return render(request, 'core/lista_atividades_mes.html', {'atividades_mes': atividade_mes})
+
+
+def lista_anualistas(request):
+    anualista = Anualista.objects.all()
+    return render(request, 'core/lista_anualistas.html', {'anualistas': anualista})
+
+
+def lista_atividades_ano(request):
+    atividade_ano = AtividadeAno.objects.all()
+    return render(request, 'core/lista_atividades_ano.html', {'atividades_ano': atividade_ano})
