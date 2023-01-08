f5c282864839a85cbb6434cb62df6555248c9c63
Rafael Trindade <trindade.rafa@gmail.com>
2023-01-08 20:22:50 -0300

    Preparando deploy de aplicacao substituta appimmovi_1

--- teste_mvp/settings.py
+++ teste_mvp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'appimmovi-teste-mvp.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'appimmovi-teste-mvp1.herokuapp.com']
 
 
 # Application definition
